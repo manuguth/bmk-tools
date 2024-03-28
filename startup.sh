83ac87599c6ed0055f79797f1ae0dc170873a3ba
Manuel Guth <manuel.guth@cern.ch>
2024-03-28 18:45:52 +0100

    Update gunicorn bind address in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 echo de_DE.UTF-8 UTF-8 >> /etc/locale.gen && locale-gen
-gunicorn --bind=0.0.0.0 --timeout 600 bmk_orders_togo.wsgi
+gunicorn --bind=0.0.0.0 --timeout 600 bmk_tools.wsgi
 # see documentation here https://docs.microsoft.com/en-us/azure/developer/python/tutorial-deploy-app-service-on-linux-04
