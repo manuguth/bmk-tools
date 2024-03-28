44a5a7047a8265dad240f9fe6ea308448c772e6c
Manuel Guth <manuel.guth@cern.ch>
2024-03-28 16:28:30 +0100

    Add production settings and configure database connections

--- manage.py
+++ manage.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bmk_tools.settings")
+    # on Azure App Service and should use the production settings.
+    settings_module = (
+        "bmk_tools.production"
+        if ("DJANGO_ENV" in os.environ and os.environ["DJANGO_ENV"] == "production")
+        else "bmk_tools.settings"
+    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
